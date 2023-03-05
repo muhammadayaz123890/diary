079162f4b31a42102b3d5791be9d0dfbfbcda073
muhammadayaz123890 <119504188+muhammadayaz123890@users.noreply.github.com>
2023-03-05 12:40:19 +0500

    Update urls.py

--- My_Diary/urls.py
+++ My_Diary/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('Src_App.urls'))
+    path('' , include('Src_App.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root' : settings.MEDIA_ROOT})
 ]
 
 
